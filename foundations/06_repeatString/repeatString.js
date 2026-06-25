8734c2a149862d9200899ca769f32d1e5b5cdf8c
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-06-24 23:41:09 -0300

    06-repeatString-complete

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let result = "";
+  if (num < 0) {
+    result = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      result += str;
+    }
+  }
+  return result;
 };
+
+console.log(repeatString("hola", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
