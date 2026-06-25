85c58f2d163274a54455cd4443e08eec3d9fae87
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-06-25 03:33:32 -0300

    11-tempConversion-complete

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let formula = Math.round(((temp - 32) / 1.8) * 10) / 10;
+  return formula;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let formula = Math.round(temp * 18) / 10 + 32;
+  return formula;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
