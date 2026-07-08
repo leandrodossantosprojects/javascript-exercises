c2b355bbb2e6c0751d89f71e80ce7278a5d38896
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-07-08 16:53:11 -0300

    16-findTheOldest finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const age = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+  let result = arr.reduce((oldest, person) => {
+    const oldestAge = age(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = age(person.yearOfBirth, person.yearOfDeath);
+    return oldestAge < currentAge ? person : oldest;
+  });
+  return result;
 };
 
 // Do not edit below this line
