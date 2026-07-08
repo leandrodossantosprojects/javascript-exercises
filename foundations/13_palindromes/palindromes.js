860e9d3bb6df11f148ed254e1cd0a86291ce2acd
Leandro Dos Santos <leandro.dossantos.projects@gmail.com>
2026-07-08 01:19:52 -0300

    13_palindromes & 14_fibonacci finished

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strClean = str.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g, "");
+  let reverseStr = strClean.split("").reverse().join("");
+  return strClean === reverseStr ? true : false;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  let indNum = Number(index);
+  if (indNum < 0) {
+    return "OOPS";
+  } else if (indNum === 0) {
+    return 0;
+  } else {
+    let secuence = [1, 1];
+    for (i = 2; i < indNum; i++) {
+      let next = secuence[i - 1] + secuence[i - 2];
+      secuence.push(next);
+    }
+    return secuence[indNum - 1];
+  }
 };
 
 // Do not edit below this line
